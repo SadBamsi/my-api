fc6956dff5ad436ccb94701c113b5bd5f58f416d
Алег <maslovoleg.by@gmail.com>
2026-02-26 22:36:04 +0100

    feat: Initialize Prisma ORM and define User model with password field

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
+const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
 });
 
+const prisma = new PrismaClient({ adapter });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the API! The server is running successfully.",
+  });
+});
+
+app.post("/api/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({ message: "Юзер создан!", id: newUser.id });
+  } catch (err) {
+    console.error("Ошибка при регистрации:", err);
+    res
+      .status(400)
+      .json({ message: "Не удалось создать юзера (может, email уже есть?)" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "Введите email и пароль" });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ message: "Неверный email или пароль" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Неверный email или пароль" });
+    }
+    res.json({ message: "Успешная авторизация", id: user.id, name: user.name });
+  } catch (err) {
+    console.error("Ошибка при входе:", err);
+    res.status(500).json({ message: "Не удалось войти" });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
